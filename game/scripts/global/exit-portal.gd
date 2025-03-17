de86b24bd330271734b5bd5247f86de941f06610
Sahand Soleimani <arsenobetaine@gmail.com>
2025-03-17 17:47:24 +0330

    Exit Portal Update

--- game/scripts/global/exit-portal.gd
+++ game/scripts/global/exit-portal.gd
@@ -8,7 +8,9 @@ func _ready() -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "player":
-		call_deferred("_change_scene")
+		var player = body as Player
+		if player and "inventory" in player and player.inventory["rubies"] == 3:
+			call_deferred("_change_scene")
 
 func _change_scene() -> void:
 	get_tree().change_scene_to_file(next_scene_path)
